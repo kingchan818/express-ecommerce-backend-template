4483a27315c6398013c0211045b0cdcfcd04a44f
calvin <chancch70@gmail.com>
2021-05-27 17:39:11 +0800

    vertifiyIsAdmin set up

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,10 +4,13 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const vertifiyIsAdmin = (req, res, next) => {
-    const token = jwt.verify(req.header('x-auth-token'), process.env.JWT_PRIVATE_KEY);
-    if (!token) return res.status(404).send('Please sign in ');
-    if (token.isAdmin) return next();
-    res.status(403).send('Forbidden');
+    try {
+        const token = jwt.verify(req.header('x-auth-token'), process.env.JWT_PRIVATE_KEY);
+        if (!token) return res.status(404).send('Please sign in ');
+        if (token.isAdmin) return next();
+    } catch (error) {
+        return res.status(403).send('Forbidden invalid signature or empty token');
+    }
 };
 
 exports.isAdmin = vertifiyIsAdmin;
