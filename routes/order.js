348a979b9d30fd210ff9c185cf04b957746b261d
calvin <chancch70@gmail.com>
2021-05-28 18:55:43 +0800

    asyncMiddleware in order api

--- routes/order.js
+++ routes/order.js
@@ -6,13 +6,17 @@ const jwt = require('jsonwebtoken');
 const { Order } = require('../models/orders');
 const { User } = require('../models/users');
 const { Product, productsValidtor } = require('../models/products');
+const asyncMiddleware = require('../middleware/async');
 
-router.get('/', async (req, res) => {
-    const token = jwt.verify(req.header('x-auth-token'), process.env.JWT_PRIVATE_KEY);
-    if (!token) return res.status(500).send('access denided please login');
-    const orders = await Order.find({ user: { _id: token._id } });
-    if (orders.length === 0) return res.send('there is no order yet');
-    res.send(orders);
-});
+router.get(
+    '/',
+    asyncMiddleware(async (req, res) => {
+        const token = jwt.verify(req.header('x-auth-token'), process.env.JWT_PRIVATE_KEY);
+        if (!token) return res.status(500).send('access denided please login');
+        const orders = await Order.find({ user: { _id: token._id } });
+        if (orders.length === 0) return res.send('there is no order yet');
+        res.send(orders);
+    })
+);
 
 module.exports = router;
