0017a2f01f15b36cecab8f2d9a716d4be86a71ef
calvin <chancch70@gmail.com>
2021-05-31 21:52:51 +0800

    delete cartItem

--- routes/cart.js
+++ routes/cart.js
@@ -1,43 +1,81 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const { CartItem } = require('../models/cartItems');
 const { Product } = require('../models/products');
+const asyncMiddleware = require('../middleware/async');
+const { forEach } = require('lodash');
 
-router.get('/', async (req, res) => {
-    const token = req.header('x-auth-token');
-    if (!token) res.status(400).send('please login first');
-    const result = await CartItem.findOne({
-        user: {
-            _id: token.id,
-        },
-    });
+router.get(
+    '/',
+    asyncMiddleware(async (req, res) => {
+        const token = jwt.verify(req.header('x-auth-token'), process.env.JWT_PRIVATE_KEY);
+        if (!token) return res.status(400).send('please login in first');
+        const result = await CartItem.findOne({
+            user: {
+                _id: token.id,
+            },
+        });
 
-    if (!result) return res.status(400).send('there is not result');
-    res.send(result);
-});
-//add product
-router.post('/add_Product', async (req, res) => {
-    const token = req.header('x-auth-token');
-    if (!token) res.status(400).send('please login first');
+        if (!result) return res.status(400).send('there is not result');
+        res.send(result);
+    })
+);
 
-    const cartItems = await CartItem.findOne({
-        user: {
-            _id: token.id,
-        },
-    });
+//update carts
+router.put(
+    '/add_Product',
+    asyncMiddleware(async (req, res) => {
+        const token = jwt.verify(req.header('x-auth-token'), process.env.JWT_PRIVATE_KEY);
+        if (!token) return res.status(400).send('please login in first');
 
-    if (!cartItems) return res.status(400).send('there are no items in your cart');
+        const cartItems = await CartItem.findOne({
+            user: {
+                _id: token.id,
+            },
+        });
 
-    const cartItem_array = cartItems.products;
+        if (!cartItems) return res.status(400).send('there are no items in your cart');
 
-    const product_obj = await Product.findById(req.body.id);
+        const cartItem_array = cartItems.products;
 
-    if (!product_obj) return res.status(400).send('there is added product');
-    cartItem_array.push(product_obj);
+        const product_obj = await Product.findById(req.body.id);
 
-    cartItems.products = cartItem_array;
-    await cartItems.save();
-    res.send(cartItems);
-});
+        if (!product_obj) return res.status(400).send('there is added product');
+        cartItem_array.push(product_obj);
+
+        cartItems.products = cartItem_array;
+        await cartItems.save();
+        res.send(cartItems);
+    })
+);
+
+// detele_cartItems
+router.put(
+    '/delete_cartItem',
+    asyncMiddleware(async (req, res) => {
+        const token = jwt.verify(req.header('x-auth-token'), process.env.JWT_PRIVATE_KEY);
+        if (!token) return res.status(400).send('please login in first');
+
+        const cart = await CartItem.findOne({
+            user: {
+                _id: token._id,
+            },
+        });
+
+        const delete_target = req.body.id;
+        const productArray = cart.products;
+
+        for (i = 0; i < productArray.length; i++) {
+            if (productArray[i]._id === delete_target) {
+                productArray.splice(i);
+            }
+        }
+        cart.products = productArray;
+        await cart.save();
+        res.send(cart);
+    })
+);
 
 module.exports = router;
